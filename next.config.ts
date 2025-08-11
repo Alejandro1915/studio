fca9725908f7013406864246ad06e1263e003b4e
Jose Gutierrez <josemoya1411@gmail.com>
2025-08-11 05:46:04 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -57,6 +57,12 @@ const nextConfig: NextConfig = {
         hostname: 'pm1.aminoapps.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
